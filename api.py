59c828a542ef8ca4ff87b1927e38b3969c59ff15
paulmunyao <paulmunyao094@gmail.com>
2022-11-22 15:52:59 +0300

    feat(students-class): a feature where you search the student name by name

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Path
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
@@ -15,7 +15,15 @@ students = {
 def index():
     return {"name": "First Data"}
 
-
+#Use a path parameter to get the student by ID
 @app.get("/get-student/{student_id}")
-def get_student(student_id: int = Path(None,description = "The ID of the student that you want to get")):
+def get_student(student_id: int = Path(None, description="The ID of the student that you want to get")):
     return students[student_id]
+
+#Use a query parameter to get the student by name
+@app.get("/get-by-name")
+def get_student(name:str):
+    for student_id in students:
+        if students[student_id]["name"] ==name:
+            return students[student_id]
+    return{"Data not found"}        
