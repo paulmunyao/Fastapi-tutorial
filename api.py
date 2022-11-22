1a05e4365bc809c5f06a307dbb6b24e4401fed6b
paulmunyao <paulmunyao094@gmail.com>
2022-11-22 10:53:01 +0300

    feat(api.py): Add a feature that will get a specific student using an id
    BREAKING CHANGE:

--- api.py
+++ api.py
@@ -2,6 +2,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+students = {
+    1:{
+        "name":"Paul",
+        "age":"17",
+        "class":"Year 12",
+    }
+}
+
 @app.get("/")
 def index():
     return {"name": "First Data"}
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id:int):
+    return students[student_id]
+
