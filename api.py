4536c70e4858f8b15da38ac521a4ebbe3fc4e375
paulmunyao <paulmunyao094@gmail.com>
2022-11-22 15:31:56 +0300

    feat(students): Add feature to describe what is required in order to get information about a particular student
    BREAKING CHANGE:

--- api.py
+++ api.py
@@ -1,20 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 
 app = FastAPI()
 
 students = {
-    1:{
-        "name":"Paul",
-        "age":"17",
-        "class":"Year 12",
+    1: {
+        "name": "Paul",
+        "age": "17",
+        "class": "Year 12",
     }
 }
+
 
 @app.get("/")
 def index():
     return {"name": "First Data"}
 
-@app.get("/get-student/{student_id}")
-def get_student(student_id:int):
-    return students[student_id]
 
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int = Path(None,description = "The ID of the student that you want to get")):
+    return students[student_id]
