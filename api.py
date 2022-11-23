912f8d144b8bb96f2245902b218b9c976de1ca18
paulmunyao <paulmunyao094@gmail.com>
2022-11-23 04:30:30 +0300

    feat(class-students): add a feature to post a new student into the system
    Use post method to achieve that functionality of adding a new student

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Path
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -10,21 +11,39 @@ students = {
     }
 }
 
+class Student(BaseModel):
+    name:str
+    age:int
+    year:int
 
 @app.get("/")
 def index():
     return {"name": "First Data"}
 
-#Use a path parameter to get the student by ID
+# Use a path parameter to get the student by ID
+
+
 @app.get("/get-student/{student_id}")
 def get_student(student_id: int = Path(None, description="The ID of the student that you want to get")):
     return students[student_id]
 
-#Use a query parameter to get the student by name
-#A feature that combines query parameter and search parameter
+# Use a query parameter to get the student by name
+# A feature that combines query parameter and search parameter
+
+
 @app.get("/get-by-name{student_id}")
-def get_student(*,name:str,student_id:int):
+def get_student(name: str, student_id: int):
     for student_id in students:
-        if students[student_id]["name"] ==name:
+        if students[student_id]["name"] == name:
             return students[student_id]
-    return{"Data not found"}        
+    return{"Data not found"}
+
+# add a new student using an ID
+
+
+@app.post("/create-student/{student_id}")
+def create_student(student_id : int, student:Student):
+    if student_id in students:
+        return {"Student already exists"}
+    students[student_id] = student
+    return students[student_id]
