6f60577d688544138a2ad446ac01cf66813a4e15
paulmunyao <paulmunyao094@gmail.com>
2022-11-23 13:40:25 +0300

    feat(class-student): Add put method to update the student details

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Path
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
@@ -11,10 +12,18 @@ students = {
     }
 }
 
+
 class Student(BaseModel):
-    name:str
-    age:int
-    year:int
+    name: str
+    age: int
+    year: int
+
+
+class UpdateStudent(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    year: Optional[int] = None
+
 
 @app.get("/")
 def index():
@@ -38,12 +47,29 @@ def get_student(name: str, student_id: int):
             return students[student_id]
     return{"Data not found"}
 
+
 # add a new student using an ID
-
-
 @app.post("/create-student/{student_id}")
-def create_student(student_id : int, student:Student):
+def create_student(student_id: int, student: Student):
     if student_id in students:
         return {"Student already exists"}
     students[student_id] = student
     return students[student_id]
+
+
+# update a student
+@app.put("/update-student/{student_id}")
+def update_student(student_id: int, student: UpdateStudent):
+    if student_id not in students:
+        return{"Error": "Student does not exist"}
+
+    if student.name != None:
+        students[student_id] = student.name
+
+    if student.age != None:
+        students[student_id] = student.age
+
+    if student.year != None:
+        students[student_id] = student.year
+
+        return students[student_id]
